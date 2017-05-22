fbee8a6e85683f8a4e6321633a58e5bd40726e9f
Fèlix Casanellas <felix.casanellas@gmail.com>
2017-05-22 12:48:04 +0200

    test

--- appinfo/app.php
+++ appinfo/app.php
@@ -10,24 +10,3 @@
  */
 
 namespace OCA\OauthClient\AppInfo;
-
-
-\OCP\App::addNavigationEntry(array(
-    // the string under which your app will be referenced in owncloud
-    'id' => 'oauthclient',
-
-    // sorting weight for the navigation. The higher the number, the higher
-    // will it be listed in the navigation
-    'order' => 10,
-
-    // the route that will be shown on startup
-    'href' => \OCP\Util::linkToRoute('oauthclient.page.index'),
-
-    // the icon that will be shown in the navigation
-    // this file needs to exist in img/
-    'icon' => \OCP\Util::imagePath('oauthclient', 'app.svg'),
-
-    // the title of your application. This will be used in the
-    // navigation or on the settings page of your app
-    'name' => 'Oauth Client'
-));
