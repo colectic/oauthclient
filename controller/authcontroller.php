9481973197fd3016c671474727ca3601cc84b49d
Fèlix Casanellas <felix.casanellas@gmail.com>
2017-05-26 16:20:38 +0200

    Groups

--- controller/authcontroller.php
+++ controller/authcontroller.php
@@ -103,8 +103,7 @@ class AuthController extends Controller {
       if ($this->userManager->userExists($uid)) {
         $user = $this->userManager->get($uid);
 				$old_groups = $this->groupManager->getUserGroupIds($user);
-
-				foreach ($new_groups as $guid) {
+				foreach ($groups as $guid) {
 					if (!in_array($guid, $old_groups)) {
 							//Add to new groups
 							$group = $this->groupManager->get($guid);
