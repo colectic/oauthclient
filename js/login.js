9f9490e115768ffeef1573795476f70015e33171
Fèlix Casanellas <felix.casanellas@gmail.com>
2017-06-23 13:55:03 +0200

    Login screen modification

--- js/login.js
+++ js/login.js
@@ -6,18 +6,22 @@
 })();
 
 $(document).ready(function(){
-
     $('<div id="login-oauth"></div>').css({
 		'text-align': 'center',
     }).appendTo('form');
 
-    $('<a id="login-saml-action" href="/index.php/apps/oauthclient" >Entra via Participa</a>').css(
+    $('<a id="login-saml-action" href="/index.php/apps/oauthclient" ><i class="fa fa-user"></i>Iniciar sessió</a>').css(
     {
-        'text-decoration': 'none',
-	'background-color': '#9C1F1D',
+      'text-decoration': 'none',
+			'background-color': '#9C1F1D',
     	'color': 'whitesmoke',
     	'padding': '5px 10px',
     	'font-weight': 'bold'
     }).appendTo('#login-oauth');
 
+		$('<a id="toggle-form" href="#">Formulari</a>').appendTo('#login-oauth');
+
+		$('#toggle-form').click(function(){
+			$('form').toggle();
+		});
 });
